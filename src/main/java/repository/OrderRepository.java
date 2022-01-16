d91861cb2bdb1e5411d6e7315388c076c2be4b5a
humastefan19 <36787065+humastefan19@users.noreply.github.com>
2022-01-16 17:38:25 +0200

    Order: WIP

--- src/main/java/repository/OrderRepository.java
+++ src/main/java/repository/OrderRepository.java
@@ -1,4 +1,15 @@
 package repository;
 
-public class OrderRepository {
+import model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import utils.OrderStatus;
+
+import java.util.Optional;
+
+public interface OrderRepository extends JpaRepository<Order,Long> {
+    @Modifying
+    @Query
+    Order updateStatus(OrderStatus status);
 }
