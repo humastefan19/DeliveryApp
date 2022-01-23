ba2bfb2f5f824b5b17d7c5f6319e255e6114c994
humastefan19 <36787065+humastefan19@users.noreply.github.com>
2022-01-23 17:08:03 +0200

    Order queries

--- src/main/java/com/example/deliveryapp/repository/OrderRepository.java
+++ src/main/java/com/example/deliveryapp/repository/OrderRepository.java
@@ -1,10 +1,16 @@
 package com.example.deliveryapp.repository;
 
 import com.example.deliveryapp.model.CustomerOrder;
+import com.example.deliveryapp.queries.OrderQuery;
+import com.example.deliveryapp.utils.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+
 
 public interface OrderRepository extends JpaRepository<CustomerOrder,Long> {
-//    @Modifying
-//    @Query(OrderQuery.updateStatusQuery)
-//    Order updateStatus(OrderStatus status, Long orderId);
+    @Modifying
+    @Query(value = OrderQuery.updateStatusQuery, nativeQuery = true)
+    CustomerOrder updateStatus(OrderStatus status, Long orderId);
 }
