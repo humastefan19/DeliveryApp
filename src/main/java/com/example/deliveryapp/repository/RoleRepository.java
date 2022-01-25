abcb8270cc0e8cdae39f2652ac185764024e147f
humastefan19 <stefan_avensis@yahoo.com>
2022-01-25 17:40:00 +0200

    Automatically assign user role

--- src/main/java/com/example/deliveryapp/repository/RoleRepository.java
+++ src/main/java/com/example/deliveryapp/repository/RoleRepository.java
@@ -1,7 +1,17 @@
 package com.example.deliveryapp.repository;
 
+import com.example.deliveryapp.model.CustomerOrder;
 import com.example.deliveryapp.model.Role;
+import com.example.deliveryapp.queries.OrderQuery;
+import com.example.deliveryapp.queries.RoleQuery;
+import com.example.deliveryapp.utils.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface RoleRepository extends JpaRepository<Role,Long> {
+    @Query(value = RoleQuery.RoleQuery, nativeQuery = true)
+    List<Role> getByName(String name);
 }
--- src/main/java/com/example/deliveryapp/service/UserServiceImpl.java
+++ src/main/java/com/example/deliveryapp/service/UserServiceImpl.java
@@ -26,7 +26,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public void save(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        user.setRoles(new HashSet<>(roleRepository.findAll()));
+        user.setRoles(new HashSet<>(roleRepository.getByName("User")));
         userRepository.save(user);
     }
 
